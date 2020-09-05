55171919885dca618642674cfbef275a5e35097e
Amit Raj <49913746+amit1884@users.noreply.github.com>
2020-09-05 12:03:06 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter,Route} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Route  exact path ="/">
+      <App/>
+    </Route>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
